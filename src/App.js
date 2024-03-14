3bcb7eea15971f2c6c90dedaeca201087133fe73
Mohammed_Wasim <38834782+wasimswdev@users.noreply.github.com>
2024-03-14 15:42:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+import video from "./images/logo.mp4";
+
+export default function App() {
+  const [positions, setPositions] = useState({
+    leftPosition: 50,
+    topPosition: 50,
+  });
+
+  const handleMouseActivity = (e) => {
+    console.log(e);
+    const canvas = document.getElementById("canvas");
+    let rect = canvas.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+    console.log(typeof x, y);
+    setPositions({
+      leftPosition: x,
+      topPosition: y,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseActivity);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseActivity);
+    };
+  });
+
+  return (
+    <main id="canvas" className="App" onMouseMove={handleMouseActivity}>
+      <video
+        style={{
+          position: "absolute",
+          left: `${positions.leftPosition}px`,
+          top: `${positions.topPosition}px`,
+        }}
+        src={video}
+        width="200"
+        height="200"
+      />
+    </main>
+  );
+}
